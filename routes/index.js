0c8c0dfb54b9928af4f15f5c3a91cf41da09ffdc
weesing <posttomichael@gmail.com>
2019-03-16 12:18:11 +0800

    Updated a few vulnerable packages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MySQL Writer Simulator' });
+});
+
+router.get('/health', function(req, res, next) {
+  res.jsonp({
+    status: 'OK'
+  });
 });
 
 module.exports = router;
